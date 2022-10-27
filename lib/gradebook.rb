3f6e5272722e193adf9df93374cdd6116d82f35f
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-27 09:01:02 -0700

    test: Add test setups for it3

---
+++ lib/gradebook.rb
@@ -0,0 +1,12 @@
+class GradeBook
+  attr_reader :instructor, :courses
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course 
+  end
+end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -8,8 +8,11 @@ RSpec.describe GradeBook do
 
   let(:student1) { Student.new({name: "Morgan", age: 21}) }
   let(:student2) { Student.new({name: "Jordan", age: 29}) }
+  let(:student3) { Student.new({name: "Brady", age: 32}) }
+  let(:student4) { Student.new({name: "Thomas", age: 25}) }
 
   let(:gradebook) { GradeBook.new("Jamison")}
+
 
   describe '#initialize' do
     it 'exists' do
@@ -19,6 +22,33 @@ RSpec.describe GradeBook do
     it 'starts with no courses' do
       expect(gradebook.courses).to eq([])
     end
+  end
 
+  describe '#add_course' do
+    it 'adds a course to gradebook' do
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+
+      expect(gradebook.courses).to eq([course1, course2])
+    end
+  end
+      
+  describe '#other_methods' do
+    before(:each) do
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student3)
+      course2.enroll(student4)
+      student1.log_score(91)
+      student1.log_score(81)
+      student2.log_score(72)
+      student2.log_score(62)
+      student3.log_score(73)
+      student3.log_score(83)
+      student4.log_score(40)
+      student4.log_score(45)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+    end
   end
 end
